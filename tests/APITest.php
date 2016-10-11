b5b7bc2e545891ae04082a94a38fde102de651d3
Esvux <usac.ing.esvin@gmail.com>
2016-10-10 18:55:18 -0600

    Ahora sí, con pruebas para el servidor de integración

---
+++ tests/APITest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class APITest extends TestCase
+{
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+
+    public function test_Login_Ok()
+    {
+        $this->json('POST', '/login', 
+            [
+                'email' => 'esvin@mail.com', 
+                'password' => '12345'
+            ])
+            ->seeJson([
+                'status' => true,
+            ]);
+    }
+
+
+    public function test_CreateUser_Ok()
+    {
+        $this->json('POST', '/users',
+            [
+                'email' => 'nuevo@mail.com', 
+                'first_name' => 'Soy Nuevo',
+                'last_name' => 'Nuevecito',
+                'password' => '123'
+            ])
+            ->seeJson(
+            [
+                'status' => true,
+            ]);
+    }
+
+
+    public function test_DeleteUser_Ok()
+    {
+        $user = App\User::where('email','nuevo@mail.com')->firstOrFail();
+        $this->json('DELETE', '/users/'.$user->id, [])
+            ->seeJson([
+                    'status' => true,
+                ]);
+    }
+
+
+}
